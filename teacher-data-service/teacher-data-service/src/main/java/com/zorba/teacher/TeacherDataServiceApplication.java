c71e77339b8ef081b003d40814aca894d58bb605
upendrari02 <upendrari02@gmail.com>
2023-05-07 15:26:54 +0530

    Add new teacher services

---
+++ teacher-data-service/teacher-data-service/src/main/java/com/zorba/teacher/TeacherDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zorba.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherDataServiceApplication.class, args);
+	}
+
+}
---
+++ teacher-data-service/teacher-data-service/src/test/java/com/zorba/teacher/TeacherDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorba.teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
